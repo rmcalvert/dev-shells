8407bd3580eb980ada486b2aac5b10d39e0a6079
Ryan Calvert <ryan.m.calvert@gmail.com>
2025-04-07 20:16:40 -0400

    Add ruby version

--- ruby/flake.nix
+++ ruby/flake.nix
@@ -2,39 +2,59 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-ruby = {
+      url = "github:bobvanderlinden/nixpkgs-ruby";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      nixpkgs-ruby,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        ruby = nixpkgs-ruby.lib.packageFromRubyVersionFile {
+          file = ./.ruby-version;
+          inherit system;
+        };
       in
+      # gems = pkgs.bundlerEnv {
+      #   name = "gemset";
+      #   inherit ruby;
+      #   gemfile = ./Gemfile;
+      #   lockfile = ./Gemfile.lock;
+      #   gemset = ./gemset.nix;
+      #   groups = [
+      #     "default"
+      #     "production"
+      #     "development"
+      #     "test"
+      #   ];
+      # };
       {
-
-        # used by nix shell and nix develop
         devShell =
           with pkgs;
           mkShell {
+            BUNDLE_PATH = ".bundle/gems";
+            GEM_HOME = ".gems";
+            GEM_PATH = ".gems";
             buildInputs = [
               ruby
-              bundler
+              # gems
               libyaml
+              # bundix
               openssl_3
               gmp
               postgresql
               redis
             ];
-            BUNDLE_PATH = ".bundle/gems";
-            GEM_HOME = ".gems";
-            GEM_PATH = ".gems";
           };
-
       }
     );
 }
